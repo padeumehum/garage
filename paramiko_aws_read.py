53d0faf383c839bbac288ce9ef223ec7de7c3b23
padeumehum <indra7112@gmail.com>
2021-08-26 21:03:01 +0900

    추가

---
+++ paramiko_aws_read.py
@@ -0,0 +1,16 @@
+import paramiko
+import time
+ 
+cli = paramiko.SSHClient()
+cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+ 
+cli.connect('호스트', username='사용자명', password='암호 or none', key_filename='/경로/LightsailDefaultKey-ap-northeast-2.pem')
+stdin, stdout, stderr = cli.exec_command("cat /경로/list.txt")
+time.sleep(2)
+lines = stdout.readlines()
+blogname=lines[3]
+cli.close()
+
+print("======")
+print(blogname)
+print("======")
